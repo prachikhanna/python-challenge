4ba1dc843b8f5cced0f6b82bd84f27449a1ff624
prachikhanna <er.prachikhanna@gmail.com>
2019-06-03 16:38:55 -0500

    95% completed

--- PyBank/Solved/main.py
+++ PyBank/Solved/main.py
@@ -12,6 +12,7 @@ with open(budget_file_path) as budget_file:
 
     print(f"CSV Header: {csv_header}")
     row_count=0
+    row_sum=0
     netprofitLoss=0
     greatestIncrease=0
     currDiff=0
@@ -22,6 +23,10 @@ with open(budget_file_path) as budget_file:
     sum=0
     for row in budget_reader:
         row_count = row_count + 1 
+        row_sum=row_sum+ int(row[1])
+        if row_count == 1 :
+           previousNumber=int(row[1])
+           continue
         netprofitLoss=netprofitLoss+int(row[1])
         currDiff=int(row[1])-previousNumber
         sum=sum+currDiff
@@ -33,21 +38,23 @@ with open(budget_file_path) as budget_file:
         if int(row[1]) >= 0:
            totalProfit = totalProfit + int(row[1])
         else:
-            totalLoss = totalLoss + int(row[1])           
+            totalLoss = totalLoss + int(row[1]) 
+    print(sum)                  
     print("Financial Analysis")
     print("----------------------------")   
     print("Total Months: " + str(row_count))
-    print("Average  Change: " )  
+    print("Total : $" + str(row_sum))
+    print("Average  Change: $" + str(round(sum/(row_count-1),2)) )  
     print("Greatest Increase in Profits: ($" + str(greatestIncrease)+ ")")   
     print("Greatest Decrease in Profits: ($" + str(greatestDecrease) + ")")   
 
     print(netprofitLoss,greatestIncrease,greatestDecrease,totalProfit,totalLoss)   
 
 with open(output_file_path,"w") as output_file:
-        # csvwriter=csv.writer(output_file ,delimiter=' ')   
         output_file.write("Financial Analysis\n")
         output_file.write("----------------------------\n")   
         output_file.write("Total Months:" + str(row_count) + "\n")
-        output_file.write("Average Change:" + "\n")  
+        output_file.write("Total : $" + str(row_sum) + "\n")    
+        output_file.write("Average Change: $" + str(round(sum/(row_count-1),2)) + "\n")  
         output_file.write("Greatest Increase in Profits: ($" + str(greatestIncrease)+ ")" + "\n")   
         output_file.write("Greatest Decrease in Profits: ($" + str(greatestDecrease) + ")" + "\n") 
